fb6d4046638267cd550a054c13b5ad0ec5a54648
Da5h1n <88321624+Da5h1n@users.noreply.github.com>
2026-01-14 13:50:34 +0000

    Refactor startup.lua to launch worker.lua
    Removed old rednet communication and added a check to run worker.lua if it exists.

--- Turtle/startup.lua
+++ Turtle/startup.lua
@@ -1,16 +1,7 @@
-rednet.open("right")
-local bridgeID = 0 -- Set this to your Bridge Computer's ID
-
-print("Turtle ID: " .. os.getComputerID())
-
-while true do
-    local id, msg = rednet.receive()
-    
-    if msg == "dance" then
-        turtle.turnRight()
-        rednet.send(id, "I am finished dancing!") -- Sends back to bridge
-    elseif msg == "status" then
-        local fuel = turtle.getFuelLevel()
-        rednet.send(id, "Fuel level is: " .. fuel)
-    end
+-- startup.lua
+-- This file rarely changes! It just launches your main code.
+if fs.exists("/TURTLE/worker.lua") then
+    shell.run("/TURTLE/worker.lua")
+else
+    print("Waiting for initial package installation...")
 end
